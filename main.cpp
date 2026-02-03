9768e4bfab3258de07a676fdb0cceccd288a3aee
naingkokolin <naingkokolin007@gmail.com>
2026-02-03 13:38:31 +0630

    added AI for enemy

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ class Character {
 
   void attack(Character &target) {
     if (attackCount <= 0) 
-      throw runtime_error("No attack left!!!!");
+      throw runtime_error("No attack left!");
     int damage = 10;
     if (target.isDefending) damage /= 2;
 
@@ -71,38 +71,63 @@ class Character {
     potionCount--;
   }
 
-  // auto attack for enemy AI
-  void autoAttack(Character &target) {
+  int randomNumber() {
     random_device rd;
     mt19937 rng(rd());
 
     uniform_int_distribution<int> dist(1, 4);
-    int choice = dist(rng);
+    int random = dist(rng);
 
-    switch (choice)
-    {
-    case 1:
-      attack(target);
-      break;
-    
-    case 2:
-      strongAttack(target);
-      break;
-
-    case 3:
-      defend();
-      break;
-    
-    case 4:
-      usePotion();
-      break;
-
-    default:
-      break;
-    }
+    return random;
   }
 
+  // auto attack for enemy AI
+  void autoAttack(Character &target) {
+    if (isFinished()) return;
+
+    bool moveMade = false;
+    while (!moveMade) {
+      int choice = randomNumber();
+
+      switch (choice) {
+        case 1:
+          if (attackCount > 0) {
+            attack(target);
+            cout << "\nEnemy used Attack!\n\n";
+            moveMade = true;
+          }
+          break;
+        case 2:
+          if (strongAttackCount > 0) {
+            strongAttack(target);
+            cout << "\nEnemy used Strong Attack!\n\n";
+            moveMade = true;
+          }
+          break;
+        case 3:
+          if (defenseCount > 0) {
+            defend();
+            cout << "\nEnemy is Defending!\n\n";
+            moveMade = true;
+          }
+          break;
+        case 4:
+          if (potionCount > 0) {
+            usePotion();
+            cout << "\nEnemy used a Potion!\n\n";
+            moveMade = true;
+          }
+          break;
+      }
+    }
+}
+
+  string getName() const { return name; }
   int getHP() const { return hp; }
+  int getAttackCount() const { return attackCount; }
+  int getStrongAttackCount() const { return strongAttackCount; }
+  int getDefenseCount() const { return defenseCount; }
+  int getPotionCount() const { return potionCount; }
 
   bool isFinished() const {
     return (attackCount == 0 && strongAttackCount == 0 && defenseCount == 0 && potionCount == 0) || hp <= 0;
@@ -112,7 +137,9 @@ class Character {
     return hp > other.hp;
   }
 
+  friend void showName(Character &c);
   friend void showHP(Character &c);
+  friend void showCounts(Character &c);
 };
 
 class Hero : public Character {
@@ -127,6 +154,14 @@ class Enemy : public Character {
 
 void showHP(Character &c) {
   cout << c.name << " HP: " << c.hp << endl;
+}
+
+template <typename T>
+void displayCount(const T &c) {
+  cout << c.getName() << " : Attack Count Left: " << c.getAttackCount() << endl;
+  cout << c.getName() << " : Strong Attack Count Left: " << c.getStrongAttackCount() << endl;
+  cout << c.getName() << " : Defense Count Left: " << c.getDefenseCount() << endl;
+  cout << c.getName() << " : Potion Count Left: " << c.getPotionCount() << endl << endl;
 }
 
 template <typename T>
@@ -174,6 +209,9 @@ int main() {
 
       showHP(hero);
       showHP(enemy);
+      cout << endl;
+      displayCount(hero);
+      displayCount(enemy);
     } catch (exception &e) {
       cout << "Error: " << e.what() << endl;
       cin.clear();
@@ -187,6 +225,9 @@ int main() {
   } else {
     cout << "Enemy Wins!\n";
   }
+
+  cout << "Hero HP: " << hero.getHP() << endl;
+  cout << "Enemy HP: " << enemy.getHP() << endl;
 
   return 0;
 }
